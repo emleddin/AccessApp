0809250fb7f2cbeffd9d3b04ff3709b763485d3f
Ian Thompson <ithompson@alynix.com>
2021-10-28 02:14:39 -0500

    Adds basic map view of reported issues and simple new report form

--- reports/urls.py
+++ reports/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ReportListView, ReportMapLayer
+from .views import ReportCreateView, ReportListView, ReportMapLayer
 from djgeojson.views import GeoJSONLayerView
 from .models import Report 
 
 urlpatterns = [
     #path('reports', ReportListView.as_view(), name='report_list'),
-    path('', ReportMapLayer.as_view(), name='data')
+    path('', ReportMapLayer.as_view(), name='data'),
+    path('new/', ReportCreateView.as_view(), name='new_report')
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from djgeojson.views import GeoJSONLayerView
+from reports.forms import ReportForm
 
 from .models import Report
 
@@ -14,3 +15,8 @@ class ReportMapLayer(GeoJSONLayerView):
     model = Report
     properties = ('id', 'description')
 
+class ReportCreateView(CreateView):
+    model = Report
+    template_name = 'create_report.html'
+    form_class = ReportForm
+    success_url = '/'
